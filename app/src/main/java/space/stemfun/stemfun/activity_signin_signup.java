eba2f969b027a266e553a5ae9fa3bafe82125503
millionlady <lejlaomerovic.biz@gmail.com>
2018-05-18 13:54:11 +0200

    New: first registration layout - student or teacher, second registration layout - sign in or sign out

---
+++ app/src/main/java/space/stemfun/stemfun/activity_signin_signup.java
@@ -0,0 +1,13 @@
+package space.stemfun.stemfun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_signin_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_signup);
+    }
+}
