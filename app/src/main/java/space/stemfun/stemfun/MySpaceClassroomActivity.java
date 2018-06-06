fd65ce144a233f9bbc8e297a4618f0fa40dc3142
millionlady <lejlaomerovic.biz@gmail.com>
2018-06-06 21:28:07 +0200

    New: files and layouts

---
+++ app/src/main/java/space/stemfun/stemfun/MySpaceClassroomActivity.java
@@ -0,0 +1,13 @@
+package space.stemfun.stemfun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MySpaceClassroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_space_classroom);
+    }
+}
