49b10b8ece7a904dc957fe25e61caef7ab34f5af
millionlady <lejlaomerovic.biz@gmail.com>
2018-06-27 16:45:04 +0200

    New: question layout

---
+++ app/src/main/java/space/stemfun/stemfun/QuestionNewActivity.java
@@ -0,0 +1,13 @@
+package space.stemfun.stemfun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionNewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_new);
+    }
+}
